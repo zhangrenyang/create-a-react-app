9e53dfd0d8176118635ba4d3e62a25e839781326
zhangrenyang <zhang_renyang@126.com>
2023-02-10 14:42:43 +0800

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+
+## 快速开始
+
+使用方式
+
+```sh
+cra [项目名称] [端口号]
+```
+
+示例 
+```sh
+# 全局安装
+$ npm install create-a-react-app@latest -g
+# 执行创建项目命令
+$ cra --projectName my-app --port 3000
+或
+$ cra my-app 3000
+```
+
+然后打开 http://localhost:3000/ 查看您的应用程序
---
+++ index.js
@@ -0,0 +1,90 @@
+#!/usr/bin/env node
+
+const yargs = require("yargs");
+const argv = yargs
+	.option("projectName", {
+		alias: "n",
+		describe: "The name of the project",
+		type: "string",
+	})
+	.option("port", {
+		alias: "p",
+		describe: "The port number",
+		type: "number",
+	})
+	.help().argv;
+
+const projectName = argv.projectName || process.argv[2] || 'my-app';
+const port = argv.port || process.argv[3] || 3000;
+
+console.log(`Project name: ${projectName}`);
+console.log(`Port: ${port}`);
+
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
+// Validate project name
+const validateProjectName = name => {
+	if (!name.match(/^[a-z0-9][a-z0-9-_.]*$/)) {
+		throw new Error(
+			"Project name can only contain lowercase letters, numbers, dots, and dashes, and can't start with a dash."
+		);
+	}
+};
+// Validate project name
+validateProjectName(projectName);
+const projectPath = path.resolve(projectName);
+// Check if project already exists
+if (fs.existsSync(projectPath)) {
+	throw new Error(`Project ${projectName} already exists.`);
+}
+// Create project path
+fs.mkdirSync(projectPath);
+// Copy template to project path
+const copyTemplate = (sourceDir, targetDir) => {
+	fs.readdirSync(sourceDir).forEach(file => {
+		const sourceFile = path.join(sourceDir, file);
+		const targetFile = path.join(targetDir, file);
+		if (fs.lstatSync(sourceFile).isDirectory()) {
+			fs.mkdirSync(targetFile);
+			copyTemplate(sourceFile, targetFile);
+		} else {
+			fs.copyFileSync(sourceFile, targetFile);
+		}
+	});
+};
+// Path to template
+const templatePath = path.join(__dirname, "template");
+// Copy template to project path
+copyTemplate(templatePath, projectPath);
+// Replace template with project name in package.json
+const packageJsonPath = path.join(projectPath, "package.json");
+let packageJsonContent = fs.readFileSync(packageJsonPath, "utf8");
+packageJsonContent = packageJsonContent.replace(/template/, projectName);
+fs.writeFileSync(packageJsonPath, packageJsonContent, "utf8");
+
+// Replace template with project name in App.js
+const appPath = path.join(projectPath, "src/App.js");
+let appContent = fs.readFileSync(appPath, "utf8");
+appContent = appContent.replace(/#projectName#/, projectName);
+fs.writeFileSync(appPath, appContent, "utf8");
+
+// Rename gitignore to .gitignore
+const gitignore = path.join(projectPath, "gitignore");
+const newGitignore = path.join(projectPath, ".gitignore");
+fs.renameSync(gitignore, newGitignore);
+
+// Create .env file
+const envPath = path.join(projectPath, ".env");
+fs.writeFileSync(envPath, `PORT = ${port}`, "utf8");
+// cd projectPath
+execSync(`cd ${projectName}`, { cwd: projectPath, stdio: "inherit" });
+// Initialize git
+execSync("npm init", { cwd: projectPath, stdio: "inherit" });
+execSync("npm add -A", { cwd: projectPath, stdio: "inherit" });
+execSync(`npm commit -m"initialization"`, { cwd: projectPath, stdio: "inherit" });
+// Install dependencies
+execSync("npm install", { cwd: projectPath, stdio: "inherit" });
+// Start project
+execSync("npm start", { cwd: projectPath, stdio: "inherit" });
+
